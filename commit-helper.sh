5315d29489bb8d6c34bc5d49d97d862880154d08
zaxchou <zaxchou@gmail.com>
2026-09-05 09:36:36 +0800

    chore: remove one-off commit helper script

--- commit-helper.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-cd /z/BaiduNetdiskWorkspace/myagent-work/zcode/projectC/calligraphy-memory
-git add -u
-TREE=$(git write-tree)
-COMMIT=$(git commit-tree "$TREE" -p HEAD -m "chore: remove one-off commit helper script (for real)")
-git update-ref refs/heads/main "$COMMIT"
-git push
-echo "pushed"
